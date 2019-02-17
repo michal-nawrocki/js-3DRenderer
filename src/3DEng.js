97c16bf6e702a8b3aa1c49ee30346bdc5de3db60
Michal.M.Nawrocki [MBP] <michal.m.nawrocki@gmail.com>
2019-02-17 21:55:49 +0000

    Added the cube model that should follow OOP standards

--- src/3DEng.js
+++ src/3DEng.js
@@ -2,21 +2,60 @@ const width = 600, height = 600;
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-var testT = triangle();
-
-testT.p[0].x = 20;
-testT.p[0].y = 30;
-testT.p[0].z = 15;
-
-var vec3d = {
-    x, 
-    y, 
-    z
+class vec3d {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 }
 
-var triangle = {
-    p = new vec3d[3]
+class triangle{
+    constructor(p0, p1, p2){
+        this.p0 = p0;
+        this.p1 = p1;
+        this.p2 = p2;
+    }
 }
+
+var cube = {
+   mesh : [
+        // SOUTH
+        new triangle( new vec3d(0.0, 0.0, 0.0),    new vec3d(0.0, 1.0, 0.0),    new vec3d(1.0, 1.0, 0.0) ),
+        new triangle( new vec3d(0.0, 0.0, 0.0),    new vec3d(1.0, 1.0, 0.0),    new vec3d(1.0, 0.0, 0.0) ),
+
+        // EAST
+        new triangle( new vec3d(1.0, 0.0, 0.0),    new vec3d(1.0, 1.0, 0.0),    new vec3d(1.0, 1.0, 1.0) ),
+        new triangle( new vec3d(1.0, 0.0, 0.0),    new vec3d(1.0, 1.0, 1.0),    new vec3d(1.0, 0.0, 1.0) ),
+
+        // NORTH
+        new triangle( new vec3d(1.0, 0.0, 1.0),    new vec3d(1.0, 1.0, 1.0),    new vec3d(0.0, 1.0, 1.0) ),
+        new triangle( new vec3d(1.0, 0.0, 1.0),    new vec3d(0.0, 1.0, 1.0),    new vec3d(0.0, 0.0, 1.0) ),
+
+        // WEST
+        new triangle( new vec3d(0.0, 0.0, 1.0),    new vec3d(0.0, 1.0, 1.0),    new vec3d(0.0, 1.0, 0.0) ),
+        new triangle( new vec3d(0.0, 0.0, 1.0),    new vec3d(0.0, 1.0, 0.0),    new vec3d(0.0, 0.0, 0.0) ),
+
+        // TOP
+        new triangle( new vec3d(0.0, 1.0, 0.0),    new vec3d(0.0, 1.0, 1.0),    new vec3d(1.0, 1.0, 1.0) ),
+        new triangle( new vec3d(0.0, 1.0, 0.0),    new vec3d(1.0, 1.0, 1.0),    new vec3d(1.0, 1.0, 0.0) ),
+
+        // BOTTOM
+        new triangle( new vec3d(1.0, 0.0, 1.0),    new vec3d(0.0, 0.0, 1.0),    new vec3d(0.0, 0.0, 0.0) ),
+        new triangle( new vec3d(1.0, 0.0, 1.0),    new vec3d(0.0, 0.0, 0.0),    new vec3d(1.0, 0.0, 0.0) )
+    ]
+}
+
+var projectionMat = {
+    fNear : 0.1,
+    fFar : 1000.0,
+    fFov : 90.0,
+    fAspectRatio : width/height,
+    fFovRad : 1 / Math.tan(fFov * 0.5 / 180.0 * 3.14159)
+
+}
+
+console.log(projectionMat.fFovRad);
 
 function OLDdrawPoint(x, y){
     ctx.moveTo(x,y);
@@ -41,6 +80,7 @@ function render(){
     //repeat the drawing
     requestAnimationFrame(render);
 }
+
 
 // Start the drawing
 render();
